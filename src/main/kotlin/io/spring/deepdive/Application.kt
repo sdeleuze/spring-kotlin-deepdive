a6a7686fc6ec8e824e50489615c9180f95ad0a81
sdeleuze <sdeleuze@pivotal.io>
2017-12-01 12:22:24 +0100

    Avoid blocking the Netty thread in the event listener

--- src/main/kotlin/io/spring/deepdive/Application.kt
+++ src/main/kotlin/io/spring/deepdive/Application.kt
@@ -1,8 +1,10 @@
 package io.spring.deepdive
 
+import com.mongodb.connection.netty.NettyStreamFactoryFactory
 import com.samskivert.mustache.Mustache.*
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.autoconfigure.mongo.MongoClientSettingsBuilderCustomizer
 import org.springframework.boot.runApplication
 import org.springframework.context.annotation.Bean
 
@@ -12,6 +14,10 @@ class Application {
     @Bean
     fun mustacheCompiler(loader: TemplateLoader) =
             compiler().escapeHTML(false).withLoader(loader)
+
+    @Bean  // Remove with Boot 2 RC1, see gh-10961
+    fun clientSettingsBuilderCustomizer() =
+            MongoClientSettingsBuilderCustomizer { it.streamFactoryFactory(NettyStreamFactoryFactory.builder().build()) }
 
 }
 
